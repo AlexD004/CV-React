126ee08e52ec2a2641fb08cb263e5d3ec1d56552
AlexD004 <dupre.alexandre04@gmail.com>
2024-05-20 15:21:06 +0200

    Add Page Portfolio and Page Blog

--- src/App.js
+++ src/App.js
@@ -16,6 +16,95 @@ import Footer from './assets/components/Footer';
 
 function App() {
 
+  // REALISATIONS
+  const realisationsCards = [
+    { 
+      id: 1,
+      imageURL: "/fresh-food.jpg",
+      alt: "Panier de légumes frais",
+      title: "Fresh Food",
+      content: <>Réalisation d'un site avec commande en ligne.</>,
+      buttonLink: "#Page-FreshFood",
+      infoPlus: "Site réalisé avec PHP et MySQL"
+    },
+    { 
+      id: 2,
+      imageURL: "/restaurant-akira.jpg",
+      alt: "Sushis et Makis",
+      title: "Restaurant Akira",
+      content: <>Réalisation d'un site vitrine.</>,
+      buttonLink: "#Page-RestaurantAkira",
+      infoPlus: "Site réalisé avec WordPress"
+    },
+    { 
+      id: 3,
+      imageURL: "/espace-bien-etre.jpg",
+      alt: "Serviette et Huiles essentielles",
+      title:"Espace bien-être",
+      content: <>Réalisation d'un site vitrine pour un praticien de bien-être.</>,
+      buttonLink: "#Page-BienEtre",
+      infoPlus: "Site réalisé avec HTML et CSS"
+    }
+  ];
+
+  // ARTICLES DE BLOG
+  const blogCards = [
+    { 
+      id: 6,
+      imageURL: "/coder.jpg",
+      alt: "Code HTML sur un écran",
+      title: "Coder son site en HTML/CSS",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-06",
+      infoPlus: "Publié le 22 août 2022"
+    },
+    { 
+      id: 5,
+      imageURL: "/e-commerce.png",
+      alt: "Wireframe d'un site e-commerce sur un ordinateur illustré",
+      title: "Vendre ses produits sur le web",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-05",
+      infoPlus: "Publié le 20 août 2022"
+    },
+    { 
+      id: 4,
+      imageURL: "/google.png",
+      alt: "Illustration d'une page Google sur un ordinateur",
+      title:"Se positionner sur Google",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-04",
+      infoPlus: "Publié le 1 août 2022"
+    },
+    { 
+      id: 3,
+      imageURL: "/responsive.png",
+      alt: "Illustration d'un zoning sur quatre tailles d'écran",
+      title:"Coder en responsive design",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-03",
+      infoPlus: "Publié le 31 juillet 2022"
+    },
+    { 
+      id: 2,
+      imageURL: "/seo.jpg",
+      alt: "Lettres S E O répétées dans une perspective",
+      title:"Techniques de référencement",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-02",
+      infoPlus: "Publié le 30 juillet 2022"
+    },
+    { 
+      id: 1,
+      imageURL: "/apprendre-code.png",
+      alt: "",
+      title:"Illustration de deux écrans, l'un avec du code et l'autre avec un wireframe",
+      content: <>Some quick example text to build on the card title and make up the bulk of the card's content.</>,
+      buttonLink: "#Article-01",
+      infoPlus: "Publié le 12 juillet 2022"
+    }
+  ];
+
   return (
     <div className="App">
       <div className="Nav">
@@ -39,8 +128,8 @@ function App() {
           <Routes>
             <Route path="/" element={ <Accueil/> }></Route>
             <Route path="/services" element={ <Services/> }></Route>
-            <Route path="/realisations" element={ <Realisations/> }></Route>
-            <Route path="/blog" element={ <Blog/> }></Route>
+            <Route path="/realisations" element={ <Realisations dataPortfolio={ realisationsCards }/> }></Route>
+            <Route path="/blog" element={ <Blog dataBlog={ blogCards }/> }></Route>
             <Route path="/contact" element={ <Contact/> }></Route>
             <Route path="/mentions-legales" element={ <MentionsLegales/> }></Route>
           </Routes>
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,13 +1,29 @@
-function Blog() {
+import CardsList from '../assets/components/CardsList'
+
+function Blog({ dataBlog }) {
+
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-          Blog
-          </h1>
+      <div className="Blog">
+        <header className="Header text-center">
+          <div className='imageHeader fluid'></div>
+          <div className='container d-flex flex-column justify-content-center align-items-center'>
+            <h1 className='mt-5 text-uppercase'>
+              Blog
+            </h1>
+            <h2>Retrouvez ici quelques articles sur le développement web.</h2>
+            <div className='underTitle bg-primary rounded my-3 mb-5'>&nbsp;</div>
+          </div>
         </header>
-        <main>
-          Main Content
+        <main className='fluid d-flex justify-content-center mb-5'>
+            <CardsList 
+              dataCards={ dataBlog } 
+              textAlign= "text-center" 
+              gutterBetweenCards= "g-2" 
+              col= "col-sm-12 col-md-6 col-lg-4" 
+              cardStyle= "cardShadow" 
+              buttonStyle= "bg-primary my-3"
+              buttonText= "Lire la suite"
+            />
         </main>
         
       </div>
